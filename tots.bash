fac228bf5d47cd7910791914b40adffb0ed76a7a
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-08-10 17:24:56 -0400

    modified:   tots.bash

--- tots.bash
+++ tots.bash
@@ -56,7 +56,7 @@ then
 else
        printf "\\n\\e[1;1;38;5;122m%s\\n\\n	%s%s\\n	%s%s\\n" "Results for ~/${RDR##*/}/cache/builtAPKs/:" "$(find "$RDR"/cache/builtAPKs/ -type f -name "*.apk" | wc -l)" " deposited in ~/${RDR##*/}/cache/builtAPKs/" "$(( $(ls -Al "$RDR/cache/builtAPKs/$JID$DAY" | wc -l) - 1 ))" " deposited in ~/${RDR##*/}/cache/builtAPKs/$JID$DAY/"
 fi
-BBSL="$(find "$RDR/scripts/bash/build/ -type f)"
+BBSL="$(find "$RDR/scripts/bash/build/" -type f)"
 printf "\\n\\e[1;1;38;5;123m%s\\n\\n%s\\n" "Build APKs (Android Package Kits) with scripts in ~/${RDR##*/}/scripts/bash/build/:" "$BBSL" 
 _WAKEUNLOCK_ ||:
 
