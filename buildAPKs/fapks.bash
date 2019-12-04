08aa11a5ebaec0239b9d39878620ef5c2ab70040
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-12-04 07:35:31 -0500

    commit 314721118 in 2019

--- buildAPKs/fapks.bash
+++ buildAPKs/fapks.bash
@@ -5,29 +5,29 @@
 #####################################################################
 set -eu
 _APKBC_() {
-	NAPKS=($(find "$JDR" -type f -name "*.apk"))
-	printf "%s" "Writing ${#NAPKS[@]} built APKs found to $JDR/var/conf/NAPKS.db  "
-	printf "%s\\n" "${#NAPKS[@]}" > "$JDR/var/conf/NAPKS.db" 
-	printf "%s" "Writing ${NAPKS[@]##*/} built APK names found to $JDR/var/conf/NAMKS.db  "
-	printf "%s\\n" "${NAPKS[@]##*/}" > "$JDR/var/conf/NAMKS.db" 
+	NAPKS=($(find "$JDR" -type f -name "*.apk")) # create array of found APK files 
+	printf "%s" "Writing ${#NAPKS[@]} built APKs found to $JDR/var/conf/NAPKS.db"
+	printf "%s\\n" "${#NAPKS[@]}" > "$JDR/var/conf/NAPKS.db" # write number of elements to file 
+	printf "%s" "Writing ${NAPKS[@]##*/} built APK names found to $JDR/var/conf/NAMKS.db"
+	printf "%s\\n" "${NAPKS[@]##*/}" > "$JDR/var/conf/NAMKS.db" # write elements to file 
 	if [ "${#NAPKS[@]}" -gt 999 ] # USENAME's work built more than 999 APKs
-	then # add USENAME string to B1000NAMES 
+	then # add USENAME string to B1000NAMES and log/B1000NAMESNAPKs files
 		_NAMESMAINBLOCK_ B1000NAMES log/B1000NAMESNAPKs
 	fi
 	if [ "${#NAPKS[@]}" -gt 99 ] # USENAME's work built more than 99 APKs
-	then # add USENAME string to B100NAMES 
+	then # add USENAME string to B100NAMES and log/B100NAMESNAPKs files 
 		_NAMESMAINBLOCK_ B100NAMES log/B100NAMESNAPKs
 	fi
 	if [ "${#NAPKS[@]}" -gt 9 ] # USENAME's work built more than 9 APKs
-	then # add USENAME string to B10NAMES
+	then # add USENAME string to B10NAMES and log/B10NAMESNAPKs files
 		_NAMESMAINBLOCK_ B10NAMES log/B10NAMESNAPKs
 	fi
 	if [ "${#NAPKS[@]}" -gt 0 ] # USENAME's work built more than 0 APKs
-	then # add USENAME string to BNAMES
+	then # add USENAME string to BNAMES and log/BNAMESNAPKs files
 		_NAMESMAINBLOCK_ BNAMES log/BNAMESNAPKs
 	fi
 	if [ "${#NAPKS[@]}" -eq 0 ] # USENAME's APKs were not built 
-	then
+	then	# check if AndroidManifest.xml files are found
 		if [[ -n $(find "$JDR" -type f -name "AndroidManifest.xml") ]] # AndroidManifest.xml files are found
 		then # add USENAME to RNAMES
 			_NAMESMAINBLOCK_ RNAMES 
