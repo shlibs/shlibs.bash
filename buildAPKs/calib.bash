3a1361378a99cc3859cd891ff5f7530962329ce7
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-12-30 15:20:59 -0500

    commit 392701772 in 2019

--- buildAPKs/calib.bash
+++ buildAPKs/calib.bash
@@ -1,12 +1,20 @@
 #!/bin/env bash
 # Copyright 2017-2019 (c) all rights reserved 
 # by S D Rausty https://sdrausty.github.io
+# Display message to terminal upon initail builds.
 #####################################################################
 set -Eeuo pipefail
 shopt -s nullglob globstar
 export RDR="$HOME/buildAPKs"
 . "$RDR"/scripts/bash/shlibs/trap.bash 116 117 118 "${0##*/} calib.bash" 
-_MAINCALIB_() {
+# create directory if not found
+[[ ! -d "$RDR"/var/tmp/ ]] && mkdir -p "$RDR"/var/tmp/
+# write initial display count to file
+[[ ! -f "$RDR"/var/tmp/CALIB.cn ]] && printf 0 > "$RDR"/var/tmp/CALIB.cn 
+# read display count from file; iterate and write display count to file
+CALIBCT="$(awk '{print $1}' "$RDR"/var/tmp/CALIB.cn)" && CALIBCT="$(( CALIBCT + 1 ))" && printf "%s" "$CALIBCT" > "$RDR"/var/tmp/CALIB.cn 
+
+_MAINCALIB_() { # print information to terminal from file .conf/LIBAUTH
 	if [[ -f "$RDR"/.conf/LIBAUTH ]] 
 	then
 		PV1="$(head -n 1 "$RDR"/.conf/LIBAUTH)"
@@ -15,8 +23,15 @@ _MAINCALIB_() {
 		then
 			printf "%s\\n" "Running \$(cat "$RDR"/.conf/LIBAUTH):"
 			printf "%s\\n" "$(cat "$RDR"/.conf/LIBAUTH)"
+			CALIBCT="$(( CALIBCT - 3 ))"
+			[[ "$CALIBCT" = -1 ]] && VIEWS="showing"|| VIEWS="showings"
+			printf "%s\\n" "${CALIBCT#-} $VIEWS of file  ~/"${RDR##*/}"/.conf/LIBAUTH remaining; Continuing..."
 		fi
 	fi
 }
-_MAINCALIB_
+
+if [[ ! "$CALIBCT" -gt 3 ]]	# display count is not greater than 3
+then	# print information to terminal 
+	_MAINCALIB_
+fi
 # calib.bash EOF
