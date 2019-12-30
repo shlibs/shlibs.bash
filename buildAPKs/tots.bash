a3b2c2652947c4570619d6968109447c408c589c
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-12-30 14:06:02 -0500

    commit 124850496 in 2019

--- buildAPKs/tots.bash
+++ buildAPKs/tots.bash
@@ -21,13 +21,13 @@ printf "\\e[1;1;38;5;119m%s\\e[0m\\n\\n" "The total increases as modules are add
 wc -l possible.total built.total | sed -n 1,2p # https://www.cyberciti.biz/faq/unix-linux-show-first-10-20-lines-of-file/
 printf "\\n\\e[1;1;38;5;120m%s\\e[0m\\n\\n" "Results for ~/${RDR##*/}/sources/$JID/:" 
 wc -l possible built | sed -n 1,2p 
-if [[ -d "/storage/emulated/0/Download/builtAPKs/$JID.$DAY" ]] || [[ -d "/storage/emulated/legacy/Download/builtAPKs/$JID.$DAY" ]]
+if [[ -w "/storage/emulated/0/" ]] ||  [[ -w "/storage/emulated/legacy/" ]]  
 then
-	if [[ -d "/storage/emulated/0/Download/builtAPKs/$JID.$DAY" ]]
+	if [[ -w "/storage/emulated/0/" ]] 
 	then
 		printf "\\n\\e[1;1;38;5;121m%s\\e[0m\\n\\n	%s%s\\n	%s%s\\n" "Results for Download/builtAPKs/:" "$(find /storage/emulated/0/Download/builtAPKs/ -type f -name "*.apk" | wc -l)" " deposited in Download/builtAPKs/" "$(( $(ls -Al "/storage/emulated/0/Download/builtAPKs/$JID.$DAY"/ | wc -l) - 1 ))" " deposited in Download/builtAPKs/$JID.$DAY/"
 	fi
-	if [[ -d "/storage/emulated/legacy/Download/builtAPKs/$JID.$DAY" ]]
+	if [[ -w "/storage/emulated/legacy/" ]]  
 	then
 		printf "\\n\\e[1;1;38;5;121m%s\\e[0m\\n\\n	%s%s\\n	%s%s\\n" "Results for Download/builtAPKs/:" "$(find /storage/emulated/legacy/Download/builtAPKs/ -type f -name "*.apk" | wc -l)" " deposited in Download/builtAPKs/" "$(( $(ls -Al "/storage/emulated/legacy/Download/builtAPKs/$JID.$DAY"/ | wc -l) - 1 ))" " deposited in Download/builtAPKs/$JID.$DAY/"
 	fi
