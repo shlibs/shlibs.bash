0651c4183036cbb3ad529b3b0a1ef953e5ae85eb
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-12-31 01:04:00 -0500

    commit 858982336 on 20191231

--- buildAPKs/fandm.bash
+++ buildAPKs/fandm.bash
@@ -4,15 +4,18 @@
 #####################################################################
 set -Eeuo pipefail
 shopt -s nullglob globstar
-. "$RDR"/scripts/bash/shlibs/trap.bash 172 173 174 "${0##*/}" 
+. "$RDR"/scripts/bash/shlibs/trap.bash 172 173 174 "${0##*/} fandm.bash" 
 _ANDB_() {
-	if [[ -z "${JDR:-}" ]]
-	then 
+	if [[ -z "${JDR:-}" ]] # JDR is not defined
+	then	# define the first arguement as JDR 
 		JDR=$1
 	fi
 	printf "\\e[1;7;38;5;222m%s\\e[0m\\n" "Searching for job directories in $JDR: Please be patient..."
-	for APP in $(find "$JDR" -type f -name AndroidManifest.xml) 
-	do 
+	AMFS=($(find "$JDR" -type f -name AndroidManifest.xml)) # create array of found AndroidManifest.xml files  
+	[[ "${#AMFS[@]}" = 1 ]] && NAMFILES="file" || NAMFILES="files" # if the number of elements in the array is one, use the singular noun form, otherwise use the plural form
+	[[ -d "$JDR/var/conf" ]] && printf "%s" "Writing ${#AMFS[@]} AndroidManifest.xml $NAMFILES found to $JDR/var/conf/NAMFS.db  " && printf "%s\\n" "${#AMFS[@]}" > "$JDR/var/conf/NAMFS.db" # if the directory $JDR/var/conf exists, write file
+	for APP in ${AMFS[@]} # all elements in array 
+	do	# process every AndroidManifest.xml file found  
 		cd "${APP%/*}" || printf "\\e[1;7;38;5;220m%s\\e[0m\\n" "Unable to find the job directory:  Continuing..." # search: string manipulation site:www.tldp.org
 		"$RDR/scripts/bash/build/build.one.bash" "${APP%/*}" 2>>"$RDR/var/log/stnderr.$JID.log" || printf "\\e[1;7;38;5;220m%s\\e[0m\\n" "Unable to parse jobs in the job directory:  Continuing..."
 	done
