3e66be8a93ecf291e6cca362185566daa9e878a6
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-12-31 22:18:42 -0500

    commit 184623079 in 2019

--- buildAPKs/tots.bash
+++ buildAPKs/tots.bash
@@ -7,8 +7,7 @@ set -Eeuo pipefail
 shopt -s nullglob globstar
 . "$RDR"/scripts/bash/shlibs/trap.bash 177 178 179 "${0##*/}" 
 . "$RDR"/scripts/bash/shlibs/buildAPKs/calib.bash
-
-BBSL="$(ls "$RDR/scripts/bash/build/")"
+BBSL="$(find "$RDR/scripts/" -name build -exec ls {} \;)"
 printf "\\n\\e[1;1;38;5;108m%s\\n%s\\e[0m\\n\\n" "Build APKs (Android Package Kits) with scripts in ~/${RDR##*/}/scripts/bash/build/:" "$BBSL" 
 mkdir -p "$TMPDIR/fa$$"
 printf "\\e[1;1;38;5;118m%s\\e[0m\\n\\n" "Calculating for ~/${RDR##*/}/.  This may take a while;  Please be patient..."
@@ -21,8 +20,9 @@ printf "\\e[1;1;38;5;119m%s\\e[0m\\n\\n" "The total increases as modules are add
 wc -l possible.total built.total | sed -n 1,2p # https://www.cyberciti.biz/faq/unix-linux-show-first-10-20-lines-of-file/
 printf "\\n\\e[1;1;38;5;120m%s\\e[0m\\n\\n" "Results for ~/${RDR##*/}/sources/$JID/:" 
 wc -l possible built | sed -n 1,2p 
+# if these directories are writable
 if [[ -w "/storage/emulated/0/Download/builtAPKs/$JID.$DAY" ]] || [[ -w "/storage/emulated/legacy/Download/builtAPKs/$JID.$DAY" ]]
-then
+then	# count the built APKs 
 	if [[ -w "/storage/emulated/0/Download/builtAPKs/$JID.$DAY" ]]
 	then
 		printf "\\n\\e[1;1;38;5;121m%s\\e[0m\\n\\n	%s%s\\n	%s%s\\n" "Results for Download/builtAPKs/:" "$(find /storage/emulated/0/Download/builtAPKs/ -type f -name "*.apk" | wc -l)" " deposited in Download/builtAPKs/" "$(( $(ls -Al "/storage/emulated/0/Download/builtAPKs/$JID.$DAY"/ | wc -l) - 1 ))" " deposited in Download/builtAPKs/$JID.$DAY/"
@@ -31,7 +31,7 @@ then
 	then
 		printf "\\n\\e[1;1;38;5;121m%s\\e[0m\\n\\n	%s%s\\n	%s%s\\n" "Results for Download/builtAPKs/:" "$(find /storage/emulated/legacy/Download/builtAPKs/ -type f -name "*.apk" | wc -l)" " deposited in Download/builtAPKs/" "$(( $(ls -Al "/storage/emulated/legacy/Download/builtAPKs/$JID.$DAY"/ | wc -l) - 1 ))" " deposited in Download/builtAPKs/$JID.$DAY/"
 	fi
-else
+else	# count the built APKs 
 	printf "\\n\\e[1;1;38;5;122m%s\\e[0m\\n\\n	%s%s\\n	%s%s\\n" "Results for ~/${RDR##*/}/var/cache/builtAPKs/:" "$(find "$RDR"/var/cache/builtAPKs/ -type f -name "*.apk" | wc -l)" " deposited in ~/${RDR##*/}/var/cache/builtAPKs/" "$(( $(ls -Al "$RDR/var/cache/builtAPKs/$JID.$DAY" | wc -l) - 1 ))" " deposited in ~/${RDR##*/}/var/cache/builtAPKs/$JID.$DAY/"
 fi
 rm -rf "$TMPDIR/fa$$"
