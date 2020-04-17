bfdf07a7d557bf3abedb39b41e23439798d10128
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-04-17 14:01:00 -0400

    commit 647414232 on 20200417

--- buildAPKs/at.bash
+++ buildAPKs/at.bash
@@ -27,28 +27,32 @@ _PTG_ () { # process a *.tar.gz file for errors
 	fi
 }
 
+_PRTCU_ () { # process a *.tar.gz file for errors
+	printf "\\n\\n\\e[1;1;38;5;190m%s%s\\e[0m\\n" "CANNOT UPDATE:  Continuing..."
+}
+
 _TB_ () { # add git modules from GitHub to cache directory
 	[ ! -d "$RDR/var/cache/tarballs" ] &&  mkdir -p "$RDR/var/cache/tarballs"
 	if [ -f "$RDR/var/cache/tarballs/$2" ]
        	then
-		(cd "$JDR/$1/" ; printf "\\n%s\\n" "Populating $JDR/$1/:" ; (tar xvf "$RDR/var/cache/tarballs/$2" | grep AndroidManifest.xml) ; export WDIR="$PWD" ; _IAR_ ) || (printf "\\n\\n\\e[1;1;38;5;190m%s%s\\e[0m\\n" "CANNOT UPDATE:  Continuing...")
+		(cd "$JDR/$1/" ; printf "\\n%s\\n" "Populating $JDR/$1/:" ; (tar xvf "$RDR/var/cache/tarballs/$2" | grep AndroidManifest.xml) ; export WDIR="$PWD" ; _IAR_ ) || _PRTCU_
 	else
 		if [ ! -z "${CULR:-}" ]
 	        then	# curl limit rate to CULR
 			cd "$RDR"/var/cache/tarballs/ ; printf "\\n%s\\n" "Getting https://github.com/$1/tarball/$2" ; curl --limit-rate $CULR -OL https://github.com/$1/tarball/$2
 			if tar tf "$RDR/var/cache/tarballs/$2" 1>/dev/null
 			then
-				_PTG_ "$2" ; cd "$JDR/$1/" ; printf "\\n%s\\n" "Populating $JDR/$1/:" ; (tar xvf "$RDR/var/cache/tarballs/$2" | grep AndroidManifest.xml) ; export WDIR="$PWD" ; _IAR_ || (printf "\\n\\n\\e[1;1;38;5;190m%s%s\\e[0m\\n" "CANNOT UPDATE:  Continuing...")
+				_PTG_ "$2" ; cd "$JDR/$1/" ; printf "\\n%s\\n" "Populating $JDR/$1/:" ; (tar xvf "$RDR/var/cache/tarballs/$2" | grep AndroidManifest.xml) ; export WDIR="$PWD" ; _IAR_ || _PRTCU_
 			else
-				printf "\\n%s\\n" "Getting https://github.com/BuildAPKs/buildAPKs.tarballs/$2" ; curl --limit-rate $CULR -OL "https://raw.githubusercontent.com/BuildAPKs/buildAPKs.tarballs/master/$2" && _PTG_ "$2" && cd "$JDR/$1/" ; printf "\\n%s\\n" "Populating $JDR/$1/:" ; (tar xvf "$RDR/var/cache/tarballs/$2" | grep AndroidManifest.xml ) ; export WDIR="$JDR/$1/" ; _IAR_ || (printf "\\n\\n\\e[1;1;38;5;190m%s%s\\e[0m\\n" "CANNOT UPDATE:  Continuing...")
+				printf "\\n%s\\n" "Getting https://github.com/BuildAPKs/buildAPKs.tarballs/$2" ; curl --limit-rate $CULR -OL "https://raw.githubusercontent.com/BuildAPKs/buildAPKs.tarballs/master/$2" && _PTG_ "$2" && cd "$JDR/$1/" ; printf "\\n%s\\n" "Populating $JDR/$1/:" ; (tar xvf "$RDR/var/cache/tarballs/$2" | grep AndroidManifest.xml ) ; export WDIR="$JDR/$1/" ; _IAR_ || _PRTCU_
 			fi
 	        else	# get files with no rate limit
 			cd "$RDR"/var/cache/tarballs/ ; printf "\\n%s\\n" "Getting https://github.com/$1/tarball/$2" ; curl -OL https://github.com/$1/tarball/$2
 			if tar tf "$RDR/var/cache/tarballs/$2" 1>/dev/null
 			then
-				_PTG_ "$2" ; cd "$JDR/$1/" ; printf "\\n%s\\n" "Populating $JDR/$1/:" ; (tar xvf "$RDR/var/cache/tarballs/$2" | grep AndroidManifest.xml) ; export WDIR="$PWD" ; _IAR_ || (printf "\\n\\n\\e[1;1;38;5;190m%s%s\\e[0m\\n" "CANNOT UPDATE:  Continuing...")
+				_PTG_ "$2" ; cd "$JDR/$1/" ; printf "\\n%s\\n" "Populating $JDR/$1/:" ; (tar xvf "$RDR/var/cache/tarballs/$2" | grep AndroidManifest.xml) ; export WDIR="$PWD" ; _IAR_ || _PRTCU_
 			else
-				printf "\\n%s\\n" "Getting https://github.com/BuildAPKs/buildAPKs.tarballs/$2" ; curl -OL "https://raw.githubusercontent.com/BuildAPKs/buildAPKs.tarballs/master/$2" && _PTG_ "$2" && cd "$JDR/$1/" ; printf "\\n%s\\n" "Populating $JDR/$1/:" ; (tar xvf "$RDR/var/cache/tarballs/$2" | grep AndroidManifest.xml ) ; export WDIR="$JDR/$1/" ; _IAR_ || (printf "\\n\\n\\e[1;1;38;5;190m%s%s\\e[0m\\n" "CANNOT UPDATE:  Continuing...")
+				printf "\\n%s\\n" "Getting https://github.com/BuildAPKs/buildAPKs.tarballs/$2" ; curl -OL "https://raw.githubusercontent.com/BuildAPKs/buildAPKs.tarballs/master/$2" && _PTG_ "$2" && cd "$JDR/$1/" ; printf "\\n%s\\n" "Populating $JDR/$1/:" ; (tar xvf "$RDR/var/cache/tarballs/$2" | grep AndroidManifest.xml ) ; export WDIR="$JDR/$1/" ; _IAR_ || _PRTCU_
 			fi
 	        fi
 	fi
