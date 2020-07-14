a8ede8eae57246002434ab6ab8dd41d1ce31ba00
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-14 19:38:36 -0400

    commit 427314178 on 20200714

---
+++ buildAPKs/doso.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# Copyright 2020 (c) all rights reserved by S D Rausty; See LICENSE
+# File `doso.bash` is under development
+#####################################################################
+set -Eeuox pipefail
+shopt -s nullglob globstar
+. "$RDR"/scripts/bash/shlibs/trap.bash 175 176 177 "${0##*/} doso.bash" 
+printf "%s\\n" "This file is under development."
+declare CPUABI=""
+CPUABI="$(getprop ro.product.cpu.abi)" 
+declare -A AMKARR # associative array
+# populate target architecture directory structure:
+PRSTARR=([arm64-v8a]=lib/arm64-v8a/libname.so [armeabi-v7a]=lib/armeabi-v7a/libname.so [x86]=lib/x86/libname.so [x86_64]=lib/x86_64/libname.so)
+echo $CPUABI
+AMKFS=($(find "$JDR" -type f -name "Android.mk")-0) # Parameter Substitution
+for FAMK in ${!AMKFS[@]}
+do 
+	if [[ $(echo $FAMK) = 0 ]]
+	then
+		printf "%s\\n" "0 Android.mk files found."
+	else
+		printf "%s\\n" "Found $FAMK."
+	fi
+done
+# doso.bash EOF
