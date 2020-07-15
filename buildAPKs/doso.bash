e730afceb295e1623235423783e862953091dd09
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-15 00:39:46 -0400

    commit 712939599 on 20200715

--- buildAPKs/doso.bash
+++ buildAPKs/doso.bash
@@ -6,8 +6,7 @@ set -Eeuo pipefail
 shopt -s nullglob globstar
 . "$RDR"/scripts/bash/shlibs/trap.bash 175 176 177 "${0##*/} doso.bash" 
 printf "%s\\n" "File \`doso.bash\` is being developed."
-_FUNZIP_()
-{
+_FUNZIP_() {
 	echo "zip -r -u "$PKGNAM.apk" "${APP%/*}/lib""
 	zip -r -u "$PKGNAM.apk" "${APP%/*}/lib"
 	echo "zip -r -u "$PKGNAM.apk" "${APP%/*}/lib": done"
@@ -25,37 +24,31 @@ then
 	echo "No CMakeLists.txt files found."
 else
 	for FAMK in ${AMKFS[@]}
-	do
-		echo 
-		echo $FAMK 
-		echo
-	done
-	for FAMK in ${AMKFS[@]}
 	do 
 		if [[ $(echo $FAMK) = 0 ]]
 		then
 			printf "%s\\n" "0 Android.mk files found."
 		else
-			printf "%s\\n" "Found $FAMK."
+			printf "%s\\n" "Found ~/$(cut -d"/" -f7-99 <<< $FAMK)."
 			cd  "${FAMK%/*}" 
-			printf "Beginning cmake && make in %s/.\\n" "$PWD"
+			printf "Beginning cmake && make in ~/%s/.\\n\\n" "$(cut -d"/" -f7-99 <<< $PWD)"
 			cmake . || printf "%s\\n" "Signal 42 gernerated in cmake ${0##*/} doso.bash"
 			make || printf "%s\\n" "Signal 44 gernerated in make ${0##*/} doso.bash"
 			SOARR=($(ls | egrep '\.o$|\.so$')) || printf "%s\\n" "Signal 46 gernerated in SOAR ${0##*/} doso.bash"
 			if [[ -z "${SOARR[@]:-}" ]]
 			then
-				printf "%s\\n" "0 *.o and *.so files were found;  There is nothing to do."
+				printf "%s\\n\\n" "0 *.o and *.so files were found;  There is nothing to do."
 			else
 				mkdir -p "${APP%/*}/lib/armeabi-v7a"
 				for i in ${SOARR[@]}
 				do
-					printf "Copying %s to %s/." "$i" "${APP%/*}/lib/armeabi-v7a"
+					printf "\\nCopying %s to ~/%s/.\\n\\n" "$i" "$(cut -d"/" -f7-99 <<< ${APP%/*})/lib/armeabi-v7a"
 					cp "$i" "${APP%/*}/lib/armeabi-v7a" || printf "%s\\n" "Signal 48 gernerated in mv ${i##*/} ${0##/*} doso.bash" 
 				done
 			fi
-			printf "\\nFinishing cmake && make in %s/.\\n" "$PWD"
+			printf "Finishing cmake && make in ~/%s/.\\n\\n" "$(cut -d"/" -f7-99 <<< $PWD)"
 			cd  "${APP%/*}"
-			printf "Change directory to %s/.\\n\\n" "$PWD"
+			printf "Change directory to ~/%s/.\\n\\n" "$(cut -d"/" -f7-99 <<< $PWD)"
 		fi
 	done
 fi
