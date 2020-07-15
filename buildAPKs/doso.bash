053dbdb52fae7e6402d5d21db23e39b2575d4e00
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-14 23:07:45 -0400

    commit 650600740 on 20200714

--- buildAPKs/doso.bash
+++ buildAPKs/doso.bash
@@ -6,20 +6,29 @@ set -Eeuo pipefail
 shopt -s nullglob globstar
 . "$RDR"/scripts/bash/shlibs/trap.bash 175 176 177 "${0##*/} doso.bash" 
 printf "%s\\n" "File \`doso.bash\` is being developed."
+_FUNZIP_()
+{
+	echo "zip -r -u "$PKGNAM.apk" "${APP%/*}/lib""
+	zip -r -u "$PKGNAM.apk" "${APP%/*}/lib"
+	echo "zip -r -u "$PKGNAM.apk" "${APP%/*}/lib": done"
+}
 declare CPUABI=""
 CPUABI="$(getprop ro.product.cpu.abi)" 
 declare -A AMKARR # associative array
 # populate target architecture directory structure:
 # PRSTARR=([arm64-v8a]=lib/arm64-v8a/libname.so [armeabi-v7a]=lib/armeabi-v7a/libname.so [x86]=lib/x86/libname.so [x86_64]=lib/x86_64/libname.so)
 printf "%s\\n" "$CPUABI"
-AMKFS=($(find "$JDR" -type f -name Android.mk -or -name CMakeLists.txt)) # Parameter Substitution
+AMKFS=($(find "$JDR" -type f -name CMakeLists.txt)) 
+# AMKFS=($(find "$JDR" -type f -name Android.mk -or -name CMakeLists.txt))
 if [[ -z "${AMKFS[@]:-}" ]]
 then
-	:
+	echo "No CMakeLists.txt files found."
 else
 	for FAMK in ${AMKFS[@]}
 	do
+		echo 
 		echo $FAMK 
+		echo
 	done
 	for FAMK in ${AMKFS[@]}
 	do 
@@ -29,11 +38,28 @@ else
 		else
 			printf "%s\\n" "Found $FAMK."
 			cd  "${FAMK%/*}" 
+			echo
 			echo "Beginning cmake && make in $PWD"
 			cmake . || printf "%s\\n" "Signal 42 gernerated in cmake ${0##/*} doso.bash"
 			make || printf "%s\\n" "Signal 44 gernerated in make ${0##/*} doso.bash"
+			echo
+			SOARR=($(ls | egrep '\.o$|\.so$')) || printf "%s\\n" "Signal 46 gernerated in SOAR ${0##/*} doso.bash"
+			if [[ -z "${SOARR[@]:-}" ]]
+			then
+				echo nothing to do
+			else
+				mkdir -p "${APP%/*}/lib/armeabi-v7a"
+				for i in ${SOARR[@]}
+				do
+					cp "$i" "${APP%/*}/lib/armeabi-v7a" || printf "%s\\n" "Signal 48 gernerated in mv ${##*/}i ${0##/*} doso.bash" 
+				done
+			fi
+			echo
+			echo "Finishing cmake && make in $PWD"
 			cd  "${APP%/*}"
-			echo "Change directory $PWD"
+			echo
+			echo "Change directory to $PWD"
+			echo
 		fi
 	done
 fi
