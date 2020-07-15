d0093efb5c40b609b6a25d63ef6f5baf1027b636
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-14 23:18:39 -0400

    commit 309264837 on 20200714

--- buildAPKs/doso.bash
+++ buildAPKs/doso.bash
@@ -51,7 +51,7 @@ else
 				mkdir -p "${APP%/*}/lib/armeabi-v7a"
 				for i in ${SOARR[@]}
 				do
-					printf "%s\\n" "Copying %s to %s/." "$i" "${APP%/*}/lib/armeabi-v7a" || printf "%s\\n" "Signal 48 gernerated in mv ${##*/}i ${0##/*} doso.bash" 
+					printf "Copying %s to %s/." "$i" "${APP%/*}/lib/armeabi-v7a" || printf "%s\\n" "Signal 48 gernerated in mv ${##*/}i ${0##/*} doso.bash" 
 					cp "$i" "${APP%/*}/lib/armeabi-v7a" || printf "%s\\n" "Signal 48 gernerated in mv ${##*/}i ${0##/*} doso.bash" 
 				done
 			fi
