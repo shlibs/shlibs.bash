1ae6c396e927dd4ad625a648fb8ff51e1758e6c3
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-21 10:15:12 -0400

    commit 301365152 on 20200721

--- buildAPKs/calib.bash
+++ buildAPKs/calib.bash
@@ -22,7 +22,7 @@ _MAINCALIB_() { # print information to terminal from file .conf/LIBAUTH
 		if [[ "$PV1" == "false" ]]
 		then
 			printf "\\e[7;38;5;122m%s\\e[0m\\n" "File ~/"${RDR##*/}"/.conf/LIBAUTH has information regarding the integration of artifacts and libraries into compilations.  The functionality of this option is being enhanced.  To improve this automation see https://github.com/BuildAPKs/buildAPKs/issues and pulls..."
-			printf "\\n\\e[2;38;5;28m%s\\e[0m\\n" "Running \$(grep -v EOF "$RDR"/.conf/LIBAUTH):"
+			printf "\\n\\e[2;38;5;28m%s\\e[0m\\n" "Running \$(tail -n +2 <<< \$(grep -v EOF "$RDR"/.conf/LIBAUTH)):"
 			printf "\\e[1;38;5;29m\\n%s\\e[0m\\n" "$(tail -n +2 <<< $(grep -v EOF "$RDR"/.conf/LIBAUTH))"
 			CALIBCT="$(( CALIBCT - 3 ))"
 			[[ "$CALIBCT" = -1 ]] && VIEWS="showing" || VIEWS="showings"
