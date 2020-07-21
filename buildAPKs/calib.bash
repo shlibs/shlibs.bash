d4aee2d605f92896298e3fc6727f41cd7f86b313
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-21 14:26:06 -0400

    commit 402217863 on 20200721

--- buildAPKs/calib.bash
+++ buildAPKs/calib.bash
@@ -21,7 +21,7 @@ _MAINCALIB_() { # print information to terminal from file .conf/LIBAUTH
 		PV1="$(awk '{print $1}' <<< $PV1)"
 		if [[ "$PV1" == "false" ]]
 		then
-			printf "\\e[7;38;5;122m%s\\e[0m\\n" "File ~/"${RDR##*/}"/.conf/LIBAUTH has information regarding the integration of artifacts and libraries into compilations.  The functionality of this option is being enhanced.  To improve this automation see https://github.com/BuildAPKs/buildAPKs/issues and pulls..."
+			printf "\\e[7;38;5;122m%s\\n%s\\e[0m\\n" "File ~/"${RDR##*/}"/.conf/GAUTH has information about enabling OAuth authentication to increase the rate limit for authenticated Github users." "File ~/"${RDR##*/}"/.conf/LIBAUTH has information regarding the integration of artifacts and libraries into compilations.  The functionality of this option is being enhanced.  To improve this automation see https://github.com/BuildAPKs/buildAPKs/issues and pulls..."
 			printf "\\n\\e[0;38;5;228m%s\\e[0m\\n" "Running \$(cat "$RDR"/.conf/GAUTH "$RDR"/.conf/LIBAUTH):"
 			printf "\\e[1;38;5;129m\\n%s\\e[0m\\n" "$(cat "$RDR"/.conf/GAUTH "$RDR"/.conf/LIBAUTH):"
 			CALIBCT="$(( CALIBCT - 3 ))"
