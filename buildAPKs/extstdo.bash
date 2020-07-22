d5458a5323d0385262a2017291af524d7413282e
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-22 10:38:13 -0400

    commit 165433153 on 20200722

--- buildAPKs/extstdo.bash
+++ buildAPKs/extstdo.bash
@@ -9,7 +9,7 @@ _EXTSTDO_() {
 _CK2EXTSTBD_() {
 	( [[ -w "$EXTSTTD/buildAPKs" ]] && printf "%s" "$FILENDSTRING found writable $EXTSTTD/buildAPKs folder : " && export EXTSTBD=0 ) || ( printf "%s" "$FILENDSTRING did not detect writable external storage $EXTSTTD/buildAPKs folder : " && _CP2EXTSTTD_ && cd "$EXTSTTD/${RDR##*/}/" && git pull && cd "$RDR" ) && export EXTSTBD=0
 	( [[ -f "$EXTSTTD/buildAPKs/.conf/VERSIONID" ]] && ESVERSIONID="$(head -n 1 $EXTSTTD/buildAPKs/.conf/VERSIONID)" && printf "%s" "$FILENDSTRING found file $EXTSTTD/buildAPKs/.conf/VERSIONID : $ESVERSIONID : " && export EXTSTBD=0 ) || ( printf "%s" "$FILENDSTRING did not find file $EXTSTTD/buildAPKs/.conf/VERSIONID : " && export EXTSTBD=1 ) 
-printf "%s\\n" "EXTSTBD is set to $EXTSTBD"
+printf "%s" "EXTSTBD is set to $EXTSTBD\\n"
 printf "%s\\n" "External storage installation $FILENDSTRING: DONE"
 }
 _CP2EXTSTTD_() {
