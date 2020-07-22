9347f1ee2a536143404aec68f0ebaf90a21c1741
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-22 01:32:02 -0400

    commit 585231017 on 20200722

--- buildAPKs/extstdo.bash
+++ buildAPKs/extstdo.bash
@@ -19,6 +19,8 @@ _CP2EXTSTTD_() {
 	cp -r "$RDR/.*" "$EXTSTTD/${RDR##*/}/"
 	printf "%s" "done "
 }
+_CK2EXTSTBD_
+}
 FILENDSTRING="${0##*/} extstdo.bash"
 [[ -f  $RDR/.conf/EXTSTDO ]] && EXTSTDO="$(head -n 1 $RDR/.conf/EXTSTDO)" && [[ $EXTSTDO = 0 ]] && _EXTSTCK_ && _EXTSTDO_
 # extstdo.bash EOF
