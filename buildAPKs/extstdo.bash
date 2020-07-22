a63b3cdebd27db3cb11fc1458805a7c5c3549544
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-22 01:26:19 -0400

    commit 983299406 on 20200722

--- buildAPKs/extstdo.bash
+++ buildAPKs/extstdo.bash
@@ -8,15 +8,16 @@ set -eu
 
 
 _EXTSTDO_() {
+_CK2EXTSTBD_() {
+	( [[ -w "$EXTSTTD/buildAPKs" ]] && printf "%s" "Found writable $EXTSTTD/buildAPKs folder : " && export EXTSTBD=0 ) || (printf "%s" "Did not detect writable external storage $EXTSTTD/buildAPKs folder : " && _CP2EXTSTTD_ && cd "$EXTSTTD/${RDR##*/}/" && git pull && cd "$RDR" )
+ 	[[ -f "$EXTSTTD/buildAPKs/.conf/VERSIONID" ]] && ESVERSIONID="$(head -n 1 $EXTSTTD/buildAPKs/.conf/VERSIONID)" && printf "%s" "Found file $EXTSTTD/buildAPKs/.conf/VERSIONID : $ESVERSIONID : " && export EXTSTBD=0 
+printf "%s\\n" "External storage installation : DONE"
+}
 _CP2EXTSTTD_() {
 	printf "%s" "Copying $RDR/ to $EXTSTTD/ : " 
 	cp -r "$RDR/" "$EXTSTTD/"
 	cp -r "$RDR/.*" "$EXTSTTD/${RDR##*/}/"
 	printf "%s" "done "
-}
-	( [[ -w "$EXTSTTD/buildAPKs" ]] && printf "%s" "Found writable $EXTSTTD/buildAPKs folder : " && export EXTSTBD=0 ) || (printf "%s" "Did not detect writable external storage $EXTSTTD/buildAPKs folder : " && _CP2EXTSTTD_ && cd "$EXTSTTD/${RDR##*/}/" && git pull && cd "$RDR" )
- 	[[ -f "$EXTSTTD/buildAPKs/.conf/VERSIONID" ]] && ESVERSIONID="$(head -n 1 $EXTSTTD/buildAPKs/.conf/VERSIONID)" && printf "%s" "Found file $EXTSTTD/buildAPKs/.conf/VERSIONID : $ESVERSIONID : " && export EXTSTBD=0 
-printf "%s\\n" "External storage installation : DONE"
 }
 FILENDSTRING="${0##*/} extstdo.bash"
 [[ -f  $RDR/.conf/EXTSTDO ]] && EXTSTDO="$(head -n 1 $RDR/.conf/EXTSTDO)" && [[ $EXTSTDO = 0 ]] && _EXTSTCK_ && _EXTSTDO_
