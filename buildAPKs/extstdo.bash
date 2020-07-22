5c5504418b4fa3cf5c81d6251a7857f3499107f0
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-22 10:17:00 -0400

    commit 719842754 on 20200722

--- buildAPKs/extstdo.bash
+++ buildAPKs/extstdo.bash
@@ -8,7 +8,7 @@ set -eu
 _EXTSTDO_() {
 _CK2EXTSTBD_() {
 	( [[ -w "$EXTSTTD/buildAPKs" ]] && printf "%s" "$FILENDSTRING found writable $EXTSTTD/buildAPKs folder : " && export EXTSTBD=0 ) || ( printf "%s" "$FILENDSTRING did not detect writable external storage $EXTSTTD/buildAPKs folder : " && _CP2EXTSTTD_ && cd "$EXTSTTD/${RDR##*/}/" && git pull && cd "$RDR" ) && export EXTSTBD=0
- 	[[ -f "$EXTSTTD/buildAPKs/.conf/VERSIONID" ]] && ESVERSIONID="$(head -n 1 $EXTSTTD/buildAPKs/.conf/VERSIONID)" && printf "%s" "$FILENDSTRING found file $EXTSTTD/buildAPKs/.conf/VERSIONID : $ESVERSIONID : " && export EXTSTBD=0 || printf "%s" "$FILENDSTRING found file $EXTSTTD/buildAPKs/.conf/VERSIONID : $ESVERSIONID : " && export EXTSTBD=1   
+ 	[[ -f "$EXTSTTD/buildAPKs/.conf/VERSIONID" ]] && ESVERSIONID="$(head -n 1 $EXTSTTD/buildAPKs/.conf/VERSIONID)" && printf "%s" "$FILENDSTRING found file $EXTSTTD/buildAPKs/.conf/VERSIONID : $ESVERSIONID : " && export EXTSTBD=0 || printf "%s" "$FILENDSTRING did not find file $EXTSTTD/buildAPKs/.conf/VERSIONID : ${ESVERSIONID:noESVERSIONID} : " && export EXTSTBD=1   
 printf "%s\\n" "EXTSTBD is set to $EXTSTBD"
 printf "%s\\n" "ESVERSIONID is set to $ESVERSIONID" 
 printf "%s\\n" "External storage installation $FILENDSTRING: DONE"
