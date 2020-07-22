8092d68c2eb0d5bef0043f0f8d1783dd98fd88d5
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-21 21:05:44 -0400

    commit 721891058 on 20200721

--- buildAPKs/extstdo.bash
+++ buildAPKs/extstdo.bash
@@ -5,25 +5,24 @@
 #####################################################################
 set -eu
 . "$RDR/scripts/bash/shlibs/android/extstck.bash" 
+
 _CP2EXTSTTD_() {
 	echo Continuing with external storage installation : Copying $RDR/ to $EXTSTTD/
 	cp -r "$RDR/" "$EXTSTTD/"
 }
 
-_EXTSTTD_() {
-	( [[ -w "$EXTSTTD/buildAPKs" ]] && echo "Detected writable external storage : Continuing with external storage feature$EXTSTTD/buildAPKs folder" && EXTSTBD=0 ) || (echo -n "Did not detect writable external storage $EXTSTTD/buildAPKs folder : Not continuing with external storage feature" && _CP2EXTSTTD_ && EXTSTBD=0 )
-	( [[ -f "$EXTSTTD/buildAPKs/.conf/VERSIONID" ]] && echo -n "Found file $EXTSTTD/buildAPKs/.conf/VERSIONID : " && EXTSTBD=0 ) || EXTSTBD=1 
-}
+	( [[ -w "$EXTSTTD/buildAPKs" ]] && echo "Detected writable external storage : Continuing with external storage feature$EXTSTTD/buildAPKs folder" && export EXTSTBD=0 ) || (echo -n "Did not detect writable external storage $EXTSTTD/buildAPKs folder : Not continuing with external storage feature" && _CP2EXTSTTD_ && export EXTSTBD=0 )
+	[[ -f "$EXTSTTD/buildAPKs/.conf/VERSIONID" ]] && ESVERSIONID="$(head -n 1 $EXTSTTD/buildAPKs/.conf/VERSIONID)" && echo -n "Found file $EXTSTTD/buildAPKs/.conf/VERSIONID : $ESVERSIONID" && export EXTSTBD=0 || EXTSTBD=1 
 
-_EXTSTDO_() {
+
 	_EXTSTCK_
+	echo "$EXTSTTD"
 	if [[ "$EXTSTBD" = 0 ]] 
 	then
-		[[ "$EXTSTCK" = 0 ]] && _EXTSTTD_
+# 		[[ "$EXTSTCK" = 0 ]] && _EXTSTTD_
 		ESVERSIONID="$(head -n 1 $EXTSTTD/buildAPKs/.conf/VERSIONID)"
 		[[ "$ESVERSIONID" = 4.7.* ]] && _CP2EXTSTTD_ || echo Not continuing with external storage installation : Version mismatch
 	else
 		echo "Could not find file $EXTSTTD/buildAPKs/.conf/VERSIONID : Not continuing with external storage installation" && EXTSTBD=1 
 	fi
-}
 # extstdo.bash EOF
