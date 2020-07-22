503133f3cd934456c1adc68a88b52c0fdf577d3a
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-22 12:15:12 -0400

    commit 160147403 on 20200722

--- android/extstck.bash
+++ android/extstck.bash
@@ -10,14 +10,14 @@ EXTSTST="${EXTSTST/emulated/}"
 EXTSTST="$(printf %s $EXTSTST)"
 EXTSTCW="$(wc -w <<< $EXTSTST)"
 EXTSTTD="/storage/$EXTSTST/Android/data/com.termux/files"
-FILENSTRING="${0##*/} extstck.bash"
+FILECKSTRING="${0##*/} extstck.bash"
 _EXTSTCK_() {
 	# check if external storage space was found
-	[[ "$EXTSTCW" = 0 ]] && printf "%s" "Did not detect writable external storage : Not continuing with external storage $FILENSTRING feature : " && export EXTSTCK=1 
+	[[ "$EXTSTCW" = 0 ]] && printf "%s" "Did not detect writable external storage : Not continuing with external storage $FILECKSTRING feature : " && export EXTSTCK=1 
 	# check if multiple external storage space were found
-	[[ "$EXTSTCW" -ge 2 ]] &&  printf "%s" "Detected multiple writable external storage spaces : Not continuing with external storage $FILENSTRING feature : This feature is being developed : " && export EXTSTCK=1 
+	[[ "$EXTSTCW" -ge 2 ]] &&  printf "%s" "Detected multiple writable external storage spaces : Not continuing with external storage $FILECKSTRING feature : This feature is being developed : " && export EXTSTCK=1 
 	# check if external storage space is writable 
-	[[ -w "$EXTSTTD" ]] &&  printf "%s" "Detected writable external storage $EXTSTTD space : $FILENSTRING continuing : " && export EXTSTCK=0  
-	printf "%s" "Check external storage installation $FILENDSTRING : DONE : "
+	[[ -w "$EXTSTTD" ]] &&  printf "%s" "Detected writable external storage $EXTSTTD space : $FILECKSTRING continuing : " && export EXTSTCK=0  
+	printf "%s" "Check external storage installation : $FILECKDSTRING DONE : "
 }
 # extstck.bash EOF
--- buildAPKs/extstdo.bash
+++ buildAPKs/extstdo.bash
@@ -5,13 +5,13 @@
 #####################################################################
 set -eu
 . "$RDR/scripts/bash/shlibs/android/extstck.bash" 
-FILENDSTRING="${0##*/} extstdo.bash"
+FILEDOSTRING="${0##*/} extstdo.bash"
 _EXTSTDO_() {
 _CK2EXTSTBD_() {
-	( [[ -w "$EXTSTTD/buildAPKs" ]] && printf "%s" "$FILENDSTRING found writable $EXTSTTD/buildAPKs folder : " && export EXTSTBD=0 ) || ( printf "%s" "$FILENDSTRING did not detect writable external storage $EXTSTTD/buildAPKs folder : " && _CP2EXTSTTD_ && cd "$EXTSTTD/${RDR##*/}/" && git pull && cd "$RDR" ) && export EXTSTBD=0
-	( [[ -f "$EXTSTTD/buildAPKs/.conf/VERSIONID" ]] && ESVERSIONID="$(head -n 1 $EXTSTTD/buildAPKs/.conf/VERSIONID)" && printf "%s" "$FILENDSTRING found file $EXTSTTD/buildAPKs/.conf/VERSIONID : $ESVERSIONID : " && export EXTSTBD=0 ) || ( printf "%s" "$FILENDSTRING did not find file $EXTSTTD/buildAPKs/.conf/VERSIONID : " && export EXTSTBD=1 ) 
+	( [[ -w "$EXTSTTD/buildAPKs" ]] && printf "%s" "$FILEDOSTRING found writable $EXTSTTD/buildAPKs folder : " && export EXTSTBD=0 ) || ( printf "%s" "$FILEDOSTRING did not detect writable external storage $EXTSTTD/buildAPKs folder : " && _CP2EXTSTTD_ && cd "$EXTSTTD/${RDR##*/}/" && git pull && cd "$RDR" ) && export EXTSTBD=0
+	( [[ -f "$EXTSTTD/buildAPKs/.conf/VERSIONID" ]] && ESVERSIONID="$(head -n 1 $EXTSTTD/buildAPKs/.conf/VERSIONID)" && printf "%s" "$FILEDOSTRING found file $EXTSTTD/buildAPKs/.conf/VERSIONID : $ESVERSIONID : " && export EXTSTBD=0 ) || ( printf "%s" "$FILEDOSTRING did not find file $EXTSTTD/buildAPKs/.conf/VERSIONID : " && export EXTSTBD=1 ) 
 printf "%s" "EXTSTBD is set to $EXTSTBD : "
-printf "%s\\n" "External storage installation $FILENDSTRING : DONE"
+printf "%s\\n" "External storage installation : $FILEDOSTRING DONE"
 }
 _CP2EXTSTTD_() {
 	printf "%s" "Copying $RDR/ to $EXTSTTD/ : " && cp -r "$RDR/" "$EXTSTTD/" 2>/dev/null && cp -r "$RDR/.*" "$EXTSTTD/${RDR##*/}/" && printf "%s" "DONE : "
