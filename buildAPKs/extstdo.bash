37fec0231c391ef21e0dd9f48abb7670ad61e9c6
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-22 11:39:59 -0400

    commit 740782418 on 20200722

--- buildAPKs/extstdo.bash
+++ buildAPKs/extstdo.bash
@@ -19,5 +19,5 @@ _CP2EXTSTTD_() {
 
 ( [[ "$EXTSTCK" = 0 ]] && _CK2EXTSTBD_ ) || :
 }
-( [[ -f  $RDR/.conf/EXTSTDO ]] && EXTSTDO="$(head -n 1 $RDR/.conf/EXTSTDO)" && [[ $EXTSTDO = 0 ]] && _EXTSTCK_ && _EXTSTDO_ ) || printf "%s" "Signal generated in L#21 extstdo.bash : $FILENDSTRING continuing : "
+( [[ -f  $RDR/.conf/EXTSTDO ]] && EXTSTDO="$(head -n 1 $RDR/.conf/EXTSTDO)" && [[ $EXTSTDO = 0 ]] && _EXTSTCK_ && _EXTSTDO_ ) || :
 # extstdo.bash EOF
