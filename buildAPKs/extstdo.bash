2e9b7b39c47ff1e1e14fbd1bdca32884e5b41863
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-21 21:35:13 -0400

    commit 925461854 on 20200721

--- buildAPKs/extstdo.bash
+++ buildAPKs/extstdo.bash
@@ -7,13 +7,13 @@ set -eu
 . "$RDR/scripts/bash/shlibs/android/extstck.bash" 
 
 _CP2EXTSTTD_() {
-	echo Continuing with external storage installation : Copying $RDR/ to $EXTSTTD/
+	echo -n Continuing with external storage installation : Copying $RDR/ to $EXTSTTD/ : 
 	cp -r "$RDR/" "$EXTSTTD/"
 }
 
 _EXTSTDO_() {
-	( [[ -w "$EXTSTTD/buildAPKs" ]] && echo "Detected writable external storage : Continuing with external storage feature$EXTSTTD/buildAPKs folder " && export EXTSTBD=0 ) || (echo -n "Did not detect writable external storage $EXTSTTD/buildAPKs folder : Not continuing with external storage feature " && _CP2EXTSTTD_ && export EXTSTBD=0 )
-	[[ -f "$EXTSTTD/buildAPKs/.conf/VERSIONID" ]] && ESVERSIONID="$(head -n 1 $EXTSTTD/buildAPKs/.conf/VERSIONID)" && echo -n "Found file $EXTSTTD/buildAPKs/.conf/VERSIONID : $ESVERSIONID " && export EXTSTBD=0 || EXTSTBD=1 
+	( [[ -w "$EXTSTTD/buildAPKs" ]] && echo -n "Detected writable external storage : Continuing with external storage feature : $EXTSTTD/buildAPKs folder " && export EXTSTBD=0 ) || (echo -n "Did not detect writable external storage $EXTSTTD/buildAPKs folder : Not continuing with external storage feature : " && _CP2EXTSTTD_ && export EXTSTBD=0 )
+	[[ -f "$EXTSTTD/buildAPKs/.conf/VERSIONID" ]] && ESVERSIONID="$(head -n 1 $EXTSTTD/buildAPKs/.conf/VERSIONID)" && echo -n "Found file $EXTSTTD/buildAPKs/.conf/VERSIONID : $ESVERSIONID : " && export EXTSTBD=0 || EXTSTBD=1 
 
 
 	_EXTSTCK_
@@ -22,11 +22,12 @@ _EXTSTDO_() {
 	then
 # 		[[ "$EXTSTCK" = 0 ]] && _EXTSTTD_
 		ESVERSIONID="$(head -n 1 $EXTSTTD/buildAPKs/.conf/VERSIONID)"
-		[[ "$ESVERSIONID" = 4.7.* ]] && _CP2EXTSTTD_ || echo "Not continuing with external storage installation : Version mismatch "
+		[[ "$ESVERSIONID" = 4.7.* ]] && _CP2EXTSTTD_ || echo -n "Not continuing with external storage installation : Version mismatch : "
 	else
-		echo "Could not find file $EXTSTTD/buildAPKs/.conf/VERSIONID : Not continuing with external storage installation " && EXTSTBD=1 
+		echo -n "Could not find file $EXTSTTD/buildAPKs/.conf/VERSIONID : Not continuing with external storage installation : " && EXTSTBD=1 
 	fi
 }
 [[ -f  $RDR/.conf/EXTSTDO ]] && EXTSTDO="$(head -n 1 $RDR/.conf/EXTSTDO)" && [[ $EXTSTDO = 0 ]] && _EXTSTDO_
 _EXTSTDO_
+echo "External storage installation : DONE"
 # extstdo.bash EOF
