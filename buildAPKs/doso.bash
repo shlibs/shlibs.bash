376b475b27fc9db6a258281f061ca49b024cd96c
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-23 11:58:29 -0400

    commit 497728048 on 20200723

--- buildAPKs/doso.bash
+++ buildAPKs/doso.bash
@@ -9,24 +9,25 @@ shopt -s nullglob globstar
 _INST_ "cmake" "cmake" "doso.bash"
 _INST_ "make" "make" "doso.bash"
 CPUABI="$(getprop ro.product.cpu.abi)"
-printf "\\e[1;38;5;113m%s\\n" "Searching for Android.mk and CMakeLists.txt files in ~/$(cut -d"/" -f7-99 <<< $JDR)/;  Please be patient..."
-AMKFS=($(find "$JDR" -type f -name Android.mk -or -name CMakeLists.txt))
+printf "\\e[1;38;5;113m%s\\n" "Searching for Android.mk and CMakeLists.txt files in ~/$(cut -d"/" -f7-99 <<< "$JDR")/;  Please be patient..."
+AMKFS=("$(find "$JDR" -type f -name Android.mk -or -name CMakeLists.txt)")
 _DOMAKES_() {
-	for FAMK in ${AMKFS[@]}
+	for FAMK in "${AMKFS[@]}"
 	do
 		if [[ $FAMK = 0 ]] # no files found
 		then
 			printf "%s\\n" "No Android.mk and CMakeLists.txt files were found; Continuing..."
 		else
-			printf "%s\\n" "Found ~/$(cut -d"/" -f7-99 <<< $FAMK)."
-			mkdir -p "$JDR/bin/lib/armeabi-v7a"
-			cp -r  "${FAMK%/*}" "$JDR/bin/lib/armeabi-v7a"
-			cd "$JDR/bin/lib/armeabi-v7a"
-			printf "Beginning cmake in ~/%s/...\\n" "$(cut -d"/" -f7-99 <<< $PWD)"
+			printf "%s\\n" "Found ~/$(cut -d"/" -f7-99 <<< "$FAMK")."
+			mkdir -p "$JDR/bin/lib/$CPUABI"
+			echo cp -r  "${FAMK%/*}/*" "$JDR/bin/lib/armeabi-v7a"
+			cp -r  "${FAMK%/*}/*" "$JDR/bin/lib/armeabi-v7a"
+			cd "$JDR/bin/lib/$CPUABI"
+			printf "Beginning cmake in ~/%s/...\\n" "$(cut -d"/" -f7-99 <<< "$PWD")"
 			cmake "${FAMK%/*}" || printf "%s\\n" "Signal 42 generated in cmake ${0##*/} doso.bash"
-			printf "Beginning make in ~/%s/...\\n" "$(cut -d"/" -f7-99 <<< $PWD)"
-			make "${FAMK%/*}" || printf "%s\\n" "Signal 44 generated in make ${0##*/} doso.bash"
-			printf "Finished cmake && make in ~/%s/.\\e[0m\\n" "$(cut -d"/" -f7-99 <<< $PWD)"
+			printf "Beginning make in ~/%s/...\\n" "$(cut -d"/" -f7-99 <<< "$PWD")"
+			make || printf "%s\\n" "Signal 44 generated in make ${0##*/} doso.bash"
+			printf "Finished cmake && make in ~/%s/.\\e[0m\\n" "$(cut -d"/" -f7-99 <<< "$PWD")"
 		fi
 	done
 }
