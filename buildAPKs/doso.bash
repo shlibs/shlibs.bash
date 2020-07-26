c54a1a7aee96152a51c42df4d8e2ff10ddca0416
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-26 18:38:34 -0400

    commit 497694427 on 20200726

--- buildAPKs/doso.bash
+++ buildAPKs/doso.bash
@@ -12,23 +12,23 @@ CPUABI="$(getprop ro.product.cpu.abi)"
 printf "\\e[1;38;5;113m%s\\n" "Searching for Android.mk and CMakeLists.txt files in ~/$(cut -d"/" -f7-99 <<< "$JDR")/;  Please be patient..."
 AMKFS=("$(find "$JDR" -type f -name Android.mk -or -name CMakeLists.txt)")
 _DOMAKES_() {
-	for FAMK in "${AMKFS[@]}"
-	do
-		if [[ $FAMK = 0 ]] # no files found
-		then
-			printf "%s\\n" "No Android.mk and CMakeLists.txt files were found; Continuing..."
-		else
-			printf "%s\\n" "Found ~/$(cut -d"/" -f7-99 <<< "$FAMK")."
-			mkdir -p "$JDR/bin/lib/$CPUABI"
-			cp -r "${FAMK%/*}/*" "$JDR/bin/lib/$CPUABI"
-			cd "$JDR/bin/lib/$CPUABI"
-			printf "%s\\n" "Beginning cmake in ~/$(cut -d"/" -f7-99 <<< "$PWD")/..."
-			cmake "${FAMK%/*}" || printf "%s\\n" "Signal 42 generated in cmake ${0##*/} doso.bash"
-			printf "%s\\n" "Beginning make in ~/$(cut -d"/" -f7-99 <<< "$PWD")/..."
-			make || printf "%s\\n" "Signal 44 generated in make ${0##*/} doso.bash"
-			printf "%s\\e[0m\\n" "Finished cmake && make in ~/$(cut -d"/" -f7-99 <<< "$PWD")/."
-		fi
-	done
+	if [[ "${#AMKFS[@]}" = 0 ]] # zero files found
+	then
+		printf "%s\\n" "Zero Android.mk and CMakeLists.txt files were found; Continuing..."
+	else
+		for FAMK in "${AMKFS[@]}"
+		do
+				printf "%s\\n" "Found ~/$(cut -d"/" -f7-99 <<< "$FAMK")."
+				mkdir -p "$JDR/bin/lib/$CPUABI"
+				cp -r "${FAMK%/*}/*" "$JDR/bin/lib/$CPUABI"
+				cd "$JDR/bin/lib/$CPUABI"
+				printf "%s\\n" "Beginning cmake in ~/$(cut -d"/" -f7-99 <<< "$PWD")/..."
+				cmake "${FAMK%/*}" || printf "\\e[1;48;5;167m%s\\e[0m\\n" "Signal 42 generated in cmake ${0##*/} doso.bash"
+				printf "%s\\n" "Beginning make in ~/$(cut -d"/" -f7-99 <<< "$PWD")/..."
+				make || printf "\\e[1;48;5;167m%s\\e[0m\\n" "Signal 44 generated in make ${0##*/} doso.bash"
+				printf "%s\\n" "Finished cmake && make in ~/$(cut -d"/" -f7-99 <<< "$PWD")/."
+		done
+	fi
 }
 if [[ -z "${AMKFS[@]:-}" ]] # is undefined
 then # no files found
