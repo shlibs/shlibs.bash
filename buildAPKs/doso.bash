b8d0eae8cc6b6e6abdc63b861b9a30e1753a665e
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-27 06:49:50 -0400

    commit 631741920 on 20200727

--- buildAPKs/doso.bash
+++ buildAPKs/doso.bash
@@ -9,34 +9,30 @@ shopt -s nullglob globstar
 _INST_ "cmake" "cmake" "${0##*/} doso.bash"
 _INST_ "make" "make" "${0##*/} doso.bash"
 CPUABI="$(getprop ro.product.cpu.abi)"
-printf "\\e[1;38;5;113m%s\\n" "Searching for Android.mk and CMakeLists.txt files in ~/$(cut -d"/" -f7-99 <<< "$JDR")/;  Please be patient..."
-AMKFS=("$(find "$JDR" -type f -name Android.mk -or -name CMakeLists.txt)")
+printf "\\e[1;38;5;113m%s\\n" "Searching for CMakeLists.txt files in ~/$(cut -d"/" -f7-99 <<< "$JDR")/;  Please be patient..."
+AMKFS=("$(find "$JDR" -type f -name CMakeLists.txt)")
 _DOMAKES_() {
-	if [[ "${#AMKFS[@]}" = 0 ]] # zero files found
-	then
-		printf "%s\\n" "Zero Android.mk and CMakeLists.txt files were found; Continuing..."
-	else
-		for FAMK in "${AMKFS[@]}"
-		do
-				printf "%s\\n" "Found ~/$(cut -d"/" -f7-99 <<< "$FAMK")."
-				UNUM="$(date +%s)"
-				sleep 1
-				mkdir -p "$JDR/bin/lib/$CPUABI/$UNUM"
- 				cp -r "${FAMK%/*}/*" "$JDR/bin/lib/$CPUABI/$UNUM"
- 				cd "$JDR/bin/lib/$CPUABI/$UNUM"
-				printf "%s\\n" "Beginning cmake in ~/$(cut -d"/" -f7-99 <<< "$PWD")/..."
-				cmake "${FAMK%/*}" || printf "\\e[1;48;5;166m%s\\e[0m\\n" "Signal 42 generated in cmake ${0##*/} doso.bash"
-				printf "%s\\n" "Beginning make in ~/$(cut -d"/" -f7-99 <<< "$PWD")/..."
-				make || printf "\\e[1;48;5;167m%s\\e[0m\\n" "Signal 44 generated in make ${0##*/} doso.bash"
-				find . -type f -name "*.so" -exec mv {} "$JDR/bin/lib/$CPUABI" \; || printf "\\e[1;48;5;168m%s\\e[0m\\n" "Signal 44 generated in find -name *.so ${0##*/} doso.bash"
-				printf "%s\\n" "Finished cmake && make in ~/$(cut -d"/" -f7-99 <<< "$PWD")/."
- 				cd "$JDR"
-		done
-	fi
+	for FAMK in ${AMKFS[@]}
+	do
+			printf "%s\\n" "Found ~/$(cut -d"/" -f7-99 <<< "$FAMK")."
+			UNUM="$(date +%s)"
+			sleep 1
+			mkdir -p "$JDR/bin/lib/$CPUABI/$UNUM"
+			cp -ar "${FAMK%/*}"/* "$JDR/bin/lib/$CPUABI/$UNUM"
+			cd "$JDR/bin/lib/$CPUABI/$UNUM"
+			pwd
+			tree || ls 
+			printf "%s\\n" "Beginning cmake and make in ~/$(cut -d"/" -f7-99 <<< "$PWD")/..."
+			cmake . || printf "\\e[1;48;5;166m%s\\e[0m\\n" "Signal 42 generated in cmake ${0##*/} doso.bash"
+			make || printf "\\e[1;48;5;167m%s\\e[0m\\n" "Signal 44 generated in make ${0##*/} doso.bash"
+			find . -type f -name "*.so" -exec mv {} "$JDR/bin/lib/$CPUABI" \; || printf "\\e[1;48;5;168m%s\\e[0m\\n" "Signal 46 generated in find -name *.so ${0##*/} doso.bash"
+			printf "%s\\n" "Finished cmake and make in ~/$(cut -d"/" -f7-99 <<< "$PWD")/."
+			cd "$JDR"
+	done
 }
 if [[ -z "${AMKFS[@]:-}" ]] # is undefined
 then # no files found
-	printf "%s\\n" "No Android.mk and CMakeLists.txt files were found; Continuing..."
+	printf "%s\\n" "No CMakeLists.txt files were found; Continuing..."
 else # call cmake and make
 	_DOMAKES_
 fi
