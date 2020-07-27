d331602eee91ff9baa3a6a8bd0c3747390da738d
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-26 21:23:15 -0400

    commit 068352619 on 20200726

--- buildAPKs/extstdo.bash
+++ buildAPKs/extstdo.bash
@@ -8,16 +8,8 @@ set -eu
 FILEDOSTRING="${0##*/} extstdo.bash"
 _EXTSTDO_() {
 _CK2EXTSTBD_() {
-	( [[ -w "$EXTSTTD/buildAPKs" ]] && printf "%s" "$FILEDOSTRING found writable $EXTSTTD/buildAPKs folder : " && EXTSTBD=0 ) || ( printf "%s" "$FILEDOSTRING did not detect writable external storage $EXTSTTD/buildAPKs folder : " && _CP2EXTSTTD_ )
+	( [[ -w "$EXTSTTD/buildAPKs" ]] && printf "%s" "$FILEDOSTRING found writable $EXTSTTD/buildAPKs folder : " && EXTSTBD=0 ) || ( printf "%s" "$FILEDOSTRING did not detect writable external storage $EXTSTTD/buildAPKs folder : " && _CP2EXTSTTD_ && _TOUCHUP_)
 	[[ -f "$EXTSTTD/buildAPKs/.conf/VERSIONID" ]] && ESVERSIONID="$(head -n 1 $EXTSTTD/buildAPKs/.conf/VERSIONID)" && printf "%s" "$FILEDOSTRING found file $EXTSTTD/buildAPKs/.conf/VERSIONID : $ESVERSIONID : " && EXTSTBD=0
-printf "%s" "Moving and linking directories : "
-mv "$RDR/var/log" "$RDR/var/cache/stash/"
-ln -s "$EXTSTTD/buildAPKs/var/log" "$RDR/var/log" 
-mv "$RDR/var/cache/tarballs" "$RDR/var/cache/stash/"
-ln -s "$EXTSTTD/buildAPKs/var/cache/tarballs" "$RDR/var/cache/tarballs"
-[[ -d $RDR/sources/github ]] && mv "$RDR/sources/github" "$RDR/var/cache/stash/" || printf "%s\\n" "Signal generated at mv $RDR/sources/github ${0##*/} extstdo.bash : Continuing : "
-[[ -d $EXTSTTD/buildAPKs/sources/github ]] && ln -s "$EXTSTTD/buildAPKs/sources/github" "$RDR/sources/github" || printf "%s\\n" "Signal generated at ln -s $EXTSTTD/buildAPKs/sources/github ${0##*/} extstdo.bash : Continuing : "
-
 printf "%s\\n" "External storage installation : $FILEDOSTRING DONE"
 }
 _CP2EXTSTTD_() {
@@ -33,6 +25,16 @@ _CP2EXTSTTD_() {
 	printf "%s" "DONE : "
 }
 
+_TOUCHUP_() {
+	printf "%s" "Moving and linking directories : "
+	mv "$RDR/var/log" "$RDR/var/cache/stash/"
+	ln -s "$EXTSTTD/buildAPKs/var/log" "$RDR/var/log" 
+	mv "$RDR/var/cache/tarballs" "$RDR/var/cache/stash/"
+	ln -s "$EXTSTTD/buildAPKs/var/cache/tarballs" "$RDR/var/cache/tarballs"
+	[[ -d $RDR/sources/github ]] && mv "$RDR/sources/github" "$RDR/var/cache/stash/" || printf "%s\\n" "Signal generated at mv $RDR/sources/github ${0##*/} extstdo.bash : Continuing : "
+	[[ -d $EXTSTTD/buildAPKs/sources/github ]] && ln -s "$EXTSTTD/buildAPKs/sources/github" "$RDR/sources/github" || printf "%s\\n" "Signal generated at ln -s $EXTSTTD/buildAPKs/sources/github ${0##*/} extstdo.bash : Continuing : "
+
+}
 [[ "$EXTSTCK" = 0 ]] && _CK2EXTSTBD_
 }
 ( [[ -f  $RDR/.conf/EXTSTDO ]] && EXTSTDO="$(head -n 1 $RDR/.conf/EXTSTDO)" && [[ $EXTSTDO = 0 ]] && _EXTSTCK_ && _EXTSTDO_ )
