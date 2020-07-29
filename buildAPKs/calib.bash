d307bb6e28b14e29e8e9b43267fafa6ff9091f74
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-29 12:01:49 -0400

    commit 252961056 on 20200729

--- buildAPKs/calib.bash
+++ buildAPKs/calib.bash
@@ -25,7 +25,8 @@ _MAINCALIB_() { # print information to terminal from file .conf/LIBAUTH
 			printf "\\n\\e[7;38;5;122m%s\\e[0m\\n" "Running \$(cat "$RDR"/.conf/GAUTH "$RDR"/.conf/LIBAUTH):"
 			printf "%s\\e[0m\\n" "$(cat "$RDR"/.conf/GAUTH "$RDR"/.conf/LIBAUTH):"
 			CALIBCT="$(( CALIBCT - 3 ))"
-			printf "\\e[7;38;5;122m%s\\e[0m\\n" "${CALIBCT#-} showings of files ~/"${RDR##*/}"/.conf/{GAUTH,LIBAUTH} remaining; Continuing..."
+			[[ "$CALIBCT" = 1 ]] && VIEWS="showings" || VIEWS="showings"
+			printf "\\e[7;38;5;122m%s\\e[0m\\n" "${CALIBCT#-} $VIEWS of files ~/"${RDR##*/}"/.conf/{GAUTH,LIBAUTH} remaining; Continuing..."
 		fi
 	fi
 }
