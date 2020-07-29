1639ed0779ec6e01e5bd77e8b48521812b3d8cbe
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-28 21:55:21 -0400

    commit 886978803 on 20200728

--- buildAPKs/doso.bash
+++ buildAPKs/doso.bash
@@ -23,8 +23,7 @@ _DOMAKES_() {
 			printf "%s\\n" "Building in $(pwd)"
 			tree || ls 
 			printf "%s\\n" "Beginning cmake and make in ~/$(cut -d"/" -f7-99 <<< "$PWD")/..."
-			cmake . || printf "\\e[1;48;5;166m%s\\e[0m\\n" "Signal 42 generated in cmake ${0##*/} doso.bash"
-			make || printf "\\e[1;48;5;167m%s\\e[0m\\n" "Signal 44 generated in make ${0##*/} doso.bash"
+			cmake . && make || printf "\\e[1;48;5;167m%s\\e[0m\\n" "Signal 42 generated in cmake && make ${0##*/} doso.bash"
 			find . -type f -name "*.so" -exec mv {} "$JDR/bin/lib/$CPUABI" \; || printf "\\e[1;48;5;168m%s\\e[0m\\n" "Signal 46 generated in find -name *.so ${0##*/} doso.bash"
 			printf "%s\\n" "Finished cmake and make in ~/$(cut -d"/" -f7-99 <<< "$PWD")/."
 			cd "$JDR"
