4600842e3c60fb9ba9b364d8282d785256785bc7
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-04-23 12:19:29 -0400

    commit 974531921 on 20210423

--- buildAPKs/copy.apk.bash
+++ buildAPKs/copy.apk.bash
@@ -6,8 +6,10 @@
 set -Eeuo pipefail
 shopt -s nullglob globstar
 _COPYAPK_ () {
-	[ ! -e /storage/emulated/0/Android/data/com.termux/builtAPKs ] && mkdir -p /storage/emulated/0/Android/data/com.termux/builtAPKs
-	[ ! -e /storage/emulated/0/Android/media/com.termux/builtAPKs ] && mkdir -p /storage/emulated/0/Android/media/com.termux/builtAPKs
+	if [ ! -e /storage/emulated/0/Android/media/com.termux/builtAPKs ]
+	then
+			mkdir /storage/emulated/0/Android/media/com.termux/builtAPKs || printf "\\e[1;1;38;5;124m%s\\e[1;1;38;5;122m%s\\e[1;32m%s\\e[0m\\n" "Could not create directory Android/media/com.termux/builtAPKs to deposit APK files.  " "Please create this directory in Android" ":  Continuing..."
+	fi
 	# if directories are writable
 	if [ -w "/storage/emulated/0/" ] || [ -w "/storage/emulated/legacy/" ]
 	then	# check if this directory is writable
@@ -26,25 +28,19 @@ _COPYAPK_ () {
 		# print copied APK file name and destination
 		printf "\\e[1;38;5;115mCopied %s to Download/builtAPKs/%s/%s.apk\\n" "$PKGNAME.apk" "$JID.$DAY" "$PKGNAME"
 		printf "\\e[1;38;5;149mThe APK %s file can be installed from Download/builtAPKs/%s/%s.apk\\n" "$PKGNAME.apk" "$JID.$DAY" "$PKGNAME"
-	elif [ -e /storage/emulated/0/Android/media/com.termux/builtAPKs ]
+	elif [ -w /storage/emulated/0/Android/media/com.termux/builtAPKs ]
 	then
-		[ ! -d "/storage/emulated/0/Android/media/com.termux/builtAPKs/$JID.$DAY" ] && mkdir -p "/storage/emulated/0/Android/media/com.termux/builtAPKs/$JID.$DAY"
+		[ ! -d "/storage/emulated/0/Android/media/com.termux/builtAPKs/$JID.$DAY" ] && mkdir "/storage/emulated/0/Android/media/com.termux/builtAPKs/$JID.$DAY"
 		cp "$PKGNAME.apk" "/storage/emulated/0/Android/media/com.termux/builtAPKs/$JID.$DAY/$PKGNAME.apk"
-		printf "\\e[1;38;5;120mCopied %s to /storage/emulated/0/Android/media/com.termux/builtAPKs/%s/%s.apk\\n" "$PKGNAME.apk" "$JID.$DAY" "$PKGNAME"
-		printf "\\e[1;38;5;154mThe APK %s file can be installed from /storage/emulated/0/Android/media/com.termux/builtAPKs/%s/%s.apk\\n" "$PKGNAME.apk" "$JID.$DAY" "$PKGNAME"
-	elif [ -e /storage/emulated/0/Android/data/com.termux/builtAPKs ]
-	then
-		[ ! -d "/storage/emulated/0/Android/data/com.termux/builtAPKs/$JID.$DAY" ] && mkdir -p "/storage/emulated/0/Android/data/com.termux/builtAPKs/$JID.$DAY"
-		cp "$PKGNAME.apk" "/storage/emulated/0/Android/data/com.termux/builtAPKs/$JID.$DAY/$PKGNAME.apk"
-		printf "\\e[1;38;5;120mCopied %s to /storage/emulated/0/Android/data/com.termux/builtAPKs/%s/%s.apk\\n" "$PKGNAME.apk" "$JID.$DAY" "$PKGNAME"
-		printf "\\e[1;38;5;154mThe APK %s file can be installed from ~/${RDR##*/}/var/cache/builtAPKs/%s/%s.apk\\n" "$PKGNAME.apk" "$JID.$DAY" "$PKGNAME"
+		printf "\\e[1;38;5;120mCopied %s to Android/media/com.termux/builtAPKs/%s/%s.apk\\n" "$PKGNAME.apk" "$JID.$DAY" "$PKGNAME"
+		printf "\\e[1;38;5;154mThe APK %s file can be installed from Android/media/com.termux/builtAPKs/%s/%s.apk\\n" "$PKGNAME.apk" "$JID.$DAY" "$PKGNAME"
 	else	# create this directory if it does not exist
 		[ ! -d "$RDR/var/cache/builtAPKs/$JID.$DAY" ] && mkdir -p "$RDR/var/cache/builtAPKs/$JID.$DAY"
 		# copy APK file to destination directory
 		cp "$PKGNAME.apk" "$RDR/var/cache/builtAPKs/$JID.$DAY/$PKGNAME.apk"
 		# print copied APK file name and destination
-		printf "\\e[1;38;5;120mCopied %s to $RDR/var/cache/builtAPKs/%s/%s.apk\\n" "$PKGNAME.apk" "$JID.$DAY" "$PKGNAME"
-		printf "\\e[1;38;5;154mThe APK %s file can be installed from ~/${RDR##*/}/var/cache/builtAPKs/%s/%s.apk\\n" "$PKGNAME.apk" "$JID.$DAY" "$PKGNAME"
+		printf "\\e[1;38;5;120mCopied %s to ~/%s/var/cache/builtAPKs/%s/%s\\n" "$PKGNAME.apk" "${RDR##*/}" "$JID.$DAY" "$PKGNAME.apk"
+		printf "\\e[1;38;5;154mThe APK %s file can be installed from ~/%s/var/cache/builtAPKs/%s/%s.apk\\n" "$PKGNAME.apk" "${RDR##*/}" "$JID.$DAY" "$PKGNAME.apk"
 	fi
 }
 # copy.apk.bash EOF
--- buildAPKs/tots.bash
+++ buildAPKs/tots.bash
@@ -20,25 +20,24 @@ printf "\\e[1;1;38;5;119m%s\\e[0m\\n\\n" "The total increases as modules are add
 wc -l possible.total built.total | sed -n 1,2p # https://www.cyberciti.biz/faq/unix-linux-show-first-10-20-lines-of-file/
 printf "\\n\\e[1;1;38;5;120m%s\\e[0m\\n\\n" "Results for ~/${RDR##*/}/sources/$JID/:"
 wc -l possible built | sed -n 1,2p
-# if these directories are writable
-if [[ -w "/storage/emulated/0/Download/builtAPKs/$JID.$DAY" ]] || [[ -w "/storage/emulated/legacy/Download/builtAPKs/$JID.$DAY" ]]
-then	# count the built APKs
-	if [[ -w "/storage/emulated/0/Download/builtAPKs/$JID.$DAY" ]]
-	then
-		printf "\\n\\e[1;1;38;5;121m%s\\e[0m\\n\\n	%s%s\\n	%s%s\\n" "Results for Download/builtAPKs/:" "$(find /storage/emulated/0/Download/builtAPKs/ -type f -name "*.apk" | wc -l)" " deposited in Download/builtAPKs/" "$(( $(ls -Al "/storage/emulated/0/Download/builtAPKs/$JID.$DAY"/ | wc -l) - 1 ))" " deposited in Download/builtAPKs/$JID.$DAY/"
-	fi
-	if [[ -w "/storage/emulated/legacy/Download/builtAPKs/$JID.$DAY" ]]
-	then
-		printf "\\n\\e[1;1;38;5;121m%s\\e[0m\\n\\n	%s%s\\n	%s%s\\n" "Results for Download/builtAPKs/:" "$(find /storage/emulated/legacy/Download/builtAPKs/ -type f -name "*.apk" | wc -l)" " deposited in Download/builtAPKs/" "$(( $(ls -Al "/storage/emulated/legacy/Download/builtAPKs/$JID.$DAY"/ | wc -l) - 1 ))" " deposited in Download/builtAPKs/$JID.$DAY/"
-	fi
-	elif [[ -w "/storage/emulated/0/Android/media/com.termux/builtAPKs/$JID.$DAY" ]]
-	then
-		printf "\\n\\e[1;1;38;5;121m%s\\e[0m\\n\\n	%s%s\\n	%s%s\\n" "Results for /storage/emulated/0/Android/media/com.termux/builtAPKs/:" "$(find /storage/emulated/0/Android/media/com.termux/builtAPKs/ -type f -name "*.apk" | wc -l)" " deposited in /storage/emulated/0/Android/media/com.termux/builtAPKs/" "$(( $(ls -Al "/storage/emulated/0/Android/media/com.termux/builtAPKs/$JID.$DAY"/ | wc -l) - 1 ))" " deposited in /storage/emulated/0/Android/media/com.termux/builtAPKs/$JID.$DAY/"
-	elif [[ -w "/storage/emulated/0/Android/data/com.termux/builtAPKs/$JID.$DAY" ]]
-	then
-		printf "\\n\\e[1;1;38;5;121m%s\\e[0m\\n\\n	%s%s\\n	%s%s\\n" "Results for /storage/emulated/0/Android/data/com.termux/builtAPKs/:" "$(find /storage/emulated/0/Android/data/com.termux/builtAPKs/ -type f -name "*.apk" | wc -l)" " deposited in /storage/emulated/0/Android/data/com.termux/builtAPKs/" "$(( $(ls -Al "/storage/emulated/0/Android/data/com.termux/builtAPKs/$JID.$DAY"/ | wc -l) - 1 ))" " deposited in /storage/emulated/0/Android/data/com.termux/builtAPKs/$JID.$DAY/"
-else	# count the built APKs
-	printf "\\n\\e[1;1;38;5;122m%s\\e[0m\\n\\n	%s%s\\n	%s%s\\n" "Results for ~/${RDR##*/}/var/cache/builtAPKs/:" "$(find "$RDR"/var/cache/builtAPKs/ -type f -name "*.apk" | wc -l)" " deposited in ~/${RDR##*/}/var/cache/builtAPKs/" "$(( $(ls -Al "$RDR/var/cache/builtAPKs/$JID.$DAY" | wc -l) - 1 ))" " deposited in ~/${RDR##*/}/var/cache/builtAPKs/$JID.$DAY/"
+# count the built APKs
+if [ -w "/storage/emulated/0/Download/builtAPKs/$JID.$DAY/" ]
+then
+	printf "\\n\\e[1;1;38;5;121m%s\\e[0m\\n\\n	%s%s\\n	%s%s\\n" "Results for Download/builtAPKs/:" "$(find /storage/emulated/0/Download/builtAPKs/ -type f -name "*.apk" | wc -l)" " deposited in Download/builtAPKs/" "$(( $(ls -Al "/storage/emulated/0/Download/builtAPKs/$JID.$DAY/" | wc -l) - 1 ))" " deposited in Download/builtAPKs/$JID.$DAY/"
+elif [ -w "/storage/emulated/legacy/Download/builtAPKs/$JID.$DAY/" ]
+then
+	printf "\\n\\e[1;1;38;5;121m%s\\e[0m\\n\\n	%s%s\\n	%s%s\\n" "Results for Download/builtAPKs/:" "$(find /storage/emulated/legacy/Download/builtAPKs/ -type f -name "*.apk" | wc -l)" " deposited in Download/builtAPKs/" "$(( $(ls -Al "/storage/emulated/legacy/Download/builtAPKs/$JID.$DAY/" | wc -l) - 1 ))" " deposited in Download/builtAPKs/$JID.$DAY/"
+elif [ -d "/storage/emulated/0/Android/media/com.termux/builtAPKs/$JID.$DAY/" ]
+then
+	printf "\\n\\e[1;1;38;5;121m%s\\e[0m\\n\\n	%s%s\\n	%s%s\\n" "Results for /storage/emulated/0/Android/media/com.termux/builtAPKs/:" "$(find /storage/emulated/0/Android/media/com.termux/builtAPKs/ -type f -name "*.apk" | wc -l)" " deposited in /storage/emulated/0/Android/media/com.termux/builtAPKs/" "$(( $(ls -Al "/storage/emulated/0/Android/media/com.termux/builtAPKs/$JID.$DAY/" | wc -l) - 1 ))" " deposited in /storage/emulated/0/Android/media/com.termux/builtAPKs/$JID.$DAY/"
+elif [ -d "/storage/emulated/0/Android/data/com.termux/builtAPKs/$JID.$DAY/" ]
+then
+	printf "\\n\\e[1;1;38;5;121m%s\\e[0m\\n\\n	%s%s\\n	%s%s\\n" "Results for /storage/emulated/0/Android/data/com.termux/builtAPKs/:" "$(find /storage/emulated/0/Android/data/com.termux/builtAPKs/ -type f -name "*.apk" | wc -l)" " deposited in /storage/emulated/0/Android/data/com.termux/builtAPKs/" "$(( $(ls -Al "/storage/emulated/0/Android/data/com.termux/builtAPKs/$JID.$DAY/" | wc -l) - 1 ))" " deposited in /storage/emulated/0/Android/data/com.termux/builtAPKs/$JID.$DAY/"
+elif [ -d "$RDR/var/cache/builtAPKs/$JID.$DAY/" ]
+then
+	printf "\\n\\e[1;1;38;5;122m%s\\e[0m\\n\\n	%s%s\\n	%s%s\\n" "Results for ~/${RDR##*/}/var/cache/builtAPKs/:" "$(find "$RDR"/var/cache/builtAPKs/ -type f -name "*.apk" | wc -l)" " deposited in ~/${RDR##*/}/var/cache/builtAPKs/" "$(( $(ls -Al "$RDR/var/cache/builtAPKs/$JID.$DAY/" | wc -l) - 1 ))" " deposited in ~/${RDR##*/}/var/cache/builtAPKs/$JID.$DAY/"
+else
+	printf "\\n\\e[1;1;38;5;124m%s\\e[1;32m%s\\e[0m\\n" "Could not tally all the results for builtAPKs:  " "Continuing..."
 fi
 rm -rf "$TMPDIR/fa$$"
 # tots.bash EOF
