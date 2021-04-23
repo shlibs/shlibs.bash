ba492aa658be3e4e2e4ffd2970dd72029d41a236
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-04-23 12:39:14 -0400

    commit 752420271 on 20210423

--- buildAPKs/copy.apk.bash
+++ buildAPKs/copy.apk.bash
@@ -31,21 +31,21 @@ _COPYAPK_ () {
 			cp "$PKGNAME.apk" "/storage/emulated/legacy/Download/builtAPKs/$JID.$DAY/$PKGNAME.apk"
 		fi
 		# print copied APK file name and destination
-		printf "\\e[1;38;5;115mCopied %s to Download/builtAPKs/%s/%s.apk\\n" "$PKGNAME.apk" "$JID.$DAY" "$PKGNAME"
-		printf "\\e[1;38;5;149mThe APK file %s can be installed from Download/builtAPKs/%s/%s.apk\\n" "$PKGNAME.apk" "$JID.$DAY" "$PKGNAME"
+		printf "\\e[1;38;5;115mCopied '%s' to 'Download/builtAPKs/%s/%s'.\\n" "$PKGNAME.apk" "$JID.$DAY" "$PKGNAME.apk"
+		printf "\\e[1;38;5;149mThe APK file '%s' can be installed from 'Download/builtAPKs/%s/%s.apk'.\\n" "$PKGNAME.apk" "$JID.$DAY" "$PKGNAME"
 	elif [ -w /storage/emulated/0/Android/media/com.termux/builtAPKs ]
 	then
 		[ ! -d "/storage/emulated/0/Android/media/com.termux/builtAPKs/$JID.$DAY" ] && mkdir "/storage/emulated/0/Android/media/com.termux/builtAPKs/$JID.$DAY"
 		cp "$PKGNAME.apk" "/storage/emulated/0/Android/media/com.termux/builtAPKs/$JID.$DAY/$PKGNAME.apk"
-		printf "\\e[1;38;5;120mCopied '%s' to Android/media/com.termux/builtAPKs/%s/%s.apk\\n" "$PKGNAME.apk" "$JID.$DAY" "$PKGNAME"
-		printf "\\e[1;38;5;154mThe APK file '%s' can be installed from Android/media/com.termux/builtAPKs/%s/%s.apk\\n" "$PKGNAME.apk" "$JID.$DAY" "$PKGNAME"
+		printf "\\e[1;38;5;120mCopied '%s' to 'Android/media/com.termux/builtAPKs/%s/%s'.\\n" "$PKGNAME.apk" "$JID.$DAY" "$PKGNAME.apk"
+		printf "\\e[1;38;5;154mThe APK file '%s' can be installed from 'Android/media/com.termux/builtAPKs/%s/%s'.\\n" "$PKGNAME.apk" "$JID.$DAY" "$PKGNAME.apk"
 	else	# create this directory if it does not exist
 		[ ! -d "$RDR/var/cache/builtAPKs/$JID.$DAY" ] && mkdir -p "$RDR/var/cache/builtAPKs/$JID.$DAY"
 		# copy APK file to destination directory
 		cp "$PKGNAME.apk" "$RDR/var/cache/builtAPKs/$JID.$DAY/$PKGNAME.apk"
 		# print copied APK file name and destination
-		printf "\\e[1;38;5;120mCopied %s to ~/%s/var/cache/builtAPKs/%s/%s\\n" "$PKGNAME.apk" "${RDR##*/}" "$JID.$DAY" "$PKGNAME.apk"
-		printf "\\e[1;38;5;154mThe APK %s file can be installed from ~/%s/var/cache/builtAPKs/%s/%s.apk\\n" "$PKGNAME.apk" "${RDR##*/}" "$JID.$DAY" "$PKGNAME.apk"
+		printf "\\e[1;38;5;120mCopied '%s' to '~/%s/var/cache/builtAPKs/%s/%s'.\\n" "$PKGNAME.apk" "${RDR##*/}" "$JID.$DAY" "$PKGNAME.apk"
+		printf "\\e[1;38;5;154mThe APK file '%s' can be installed from '~/%s/var/cache/builtAPKs/%s/%s'.\\n" "$PKGNAME.apk" "${RDR##*/}" "$JID.$DAY" "$PKGNAME.apk"
 	fi
 }
 # copy.apk.bash EOF
