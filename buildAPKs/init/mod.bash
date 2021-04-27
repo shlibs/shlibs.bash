c263cd0c403a252be0a2e2591e90157abc102e00
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-04-27 14:29:02 -0400

    commit 495768606 on 20210427

--- buildAPKs/init/mod.bash
+++ buildAPKs/init/mod.bash
@@ -68,7 +68,7 @@ _MAINMODS_ () {
 	# create directories in RDR if not exist
 	_MKDIRS_ "bin" "opt" "tmp" "var/cache/lib" "var/cache/lib/res-appcompat" "var/cache/lib/res-cardview" "var/cache/lib/res-design" "var/cache/lib/res-recyclerview" "var/cache/stash" "var/cache/tarballs" "var/db" "var/db/log" "var/lock" "var/log/github/orgs" "var/log/github/users" "var/log/messages" "var/log/messages" "var/run/lock/auth" "var/run/lock/wake" "var/tmp"                              
 	# create files in RDR/var if not exist
-	_MKFILES_ "db/BNAMES" "db/B10NAMES" "db/B100NAMES" "db/ENAMES" "db/GNAMES" "db/QNAMES" "db/XNAMES" "db/YNAMES" "db/ZNAMES"
+	_MKVFILES_ "db/BNAMES" "db/B10NAMES" "db/B100NAMES" "db/ENAMES" "db/GNAMES" "db/QNAMES" "db/XNAMES" "db/YNAMES" "db/ZNAMES"
 	# create symlinks in RDR if not exist
 	_CLINKS_
 	if [ -f "$JDR/.git" ] # file exists in job directory
