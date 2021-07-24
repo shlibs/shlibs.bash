99d7955ddd1b35c76d0dad96be9076ba548c1708
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-07-24 09:42:12 -0400

    commit 952683058 on 20210724

--- buildAPKs/init/mod.bash
+++ buildAPKs/init/mod.bash
@@ -84,7 +84,7 @@ _MAINMODS_ () {
 }
 
 _PRINTUMODS_() {
-	printf "\\e[1;2;38;5;190m%s \\e[1;2;38;5;180m%s \\e[1;2;38;5;190m%s%s\\e[0m\\n\\n" "Updating buildAPKs: '${0##*/} mod.bash' is" "loading sources from repository submodule https://$JAD" " into buildAPKs.  This may take a little while to complete.  Please be patient if this script wants to download source code from https://github.com:"
+	printf "\\e[1;2;38;5;190m%s \\e[1;2;38;5;180m%s \\e[1;2;38;5;190m%s%s\\e[0m\\n\\n" "Updating buildAPKs: '${0##*/} mod.bash' is" "loading sources from repository submodule https://$JAD" "into buildAPKs.  This may take a little while to complete.  Please be patient if this script wants to download source code from https://github.com:"
 }
 
 _PRINTUSAGE_() {
