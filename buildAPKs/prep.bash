b12b922f90e12f03afe354924d8aed68301abcf6
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-09-27 07:34:48 -0400

    commit 665158177 on 20210927

--- buildAPKs/prep.bash
+++ buildAPKs/prep.bash
@@ -39,7 +39,7 @@ _AFR_ () { # finds and removes superfluous directories and files
 	do
 		(find "$WDIR" -type f -name "$NAME" -delete 2>/dev/null && ([ -f "$JDR/sha512.sum" ] && grep "$NAME" "$JDR/sha512.sum" 1>/dev/null && sed -i "/$NAME/d" "$JDR/sha512.sum" ))
 	done
-	[ ! -f "$RDR/var/tmp/$JID.sha512.0" ] && [ -f "$JDR/sha512.sum" ] && { CWDIRPWD="$PWD" && cd "$JDR" && printf "\\e[1;2m%s" "Running 'sha512sum --quiet -c sha512.sum' in directory '$PWD': $(sha512sum --quiet -c sha512.sum)" && cd "$CWDIRPWD" && touch "$RDR/var/lock/$JID.sha512.lock" && printf "\\e[1;32mDONE\\e[0m\\n" ; }
+	[ ! -f "$RDR/var/tmp/$JID.sha512.0" ] && [ -f "$JDR/sha512.sum" ] && { CWDIRPWD="$PWD" && cd "$JDR" && printf "\\e[1;2m%s" "Running 'sha512sum --quiet -c sha512.sum' in directory '$PWD': $(sha512sum --quiet -c sha512.sum)" && cd "$CWDIRPWD" && touch "$RDR/var/tmp/$JID.sha512.lock" && printf "\\e[1;32mDONE\\e[0m\\n" ; }
 	printf "\\e[?25h\\e[1;48;5;101mBuildAPKs %s\\e[0m\\n" "${0##*/} prep.bash $WDIR: DONE"
 }
 
